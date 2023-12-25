cabaf3c169fbc5b1d34d12137998100f34d778c4
moezl <moez.mltifi@gmail.com>
2023-12-25 14:59:55 +0100

    fix play again

--- dice.js
+++ dice.js
@@ -2,34 +2,30 @@
 document.getElementById("refresh").onclick= getRandomValues
 var scorePlayer1=0
 var scorePlayer2=0
-var numberOfRound=0
+var numberOfRoundsPlayed=0
+let maxNumberOfRounds=5
 
 
-
-var palyer1 = prompt("what is the first player's name?")
-var palyer2 = prompt("what is the second player's name?")
-let Round=prompt("how many rounds you want to play")
+var palyer1 = prompt("what is the first player's name?","player1")
+var palyer2 = prompt("what is the second player's name?","player2")
 
 document.getElementById("Playern1").innerHTML =palyer1
 document.getElementById("Playern2").innerHTML =palyer2
 
-document.getElementById("NOfR").innerHTML =Round
+document.getElementById("NOfR").innerHTML =maxNumberOfRounds
 
 document.getElementById("reset").onclick=reset
 
 function reset(){
-  numberOfRound=0
+  numberOfRoundsPlayed=0
   scorePlayer1=0
   scorePlayer2=0
-  document.getElementById("alert2").innerHTML=""
   document.getElementById("alert1").innerHTML=""
-  document.getElementById("AcR").innerHTML =numberOfRound
+  document.getElementById("AcR").innerHTML =numberOfRoundsPlayed
   document.getElementById("Playern1").innerHTML =""
   document.getElementById("Playern2").innerHTML =""
-  document.getElementById("Playern1").innerHTML =prompt("what is the first player's name?")
-  document.getElementById("Playern2").innerHTML =prompt("what is the second player's name?")
-  Round=prompt("how many rounds you want to play")
-  document.getElementById("NOfR").innerHTML =Round
+  document.getElementById("Playern1").innerHTML =prompt("what is the first player's name?","player1")
+  document.getElementById("Playern2").innerHTML =prompt("what is the second player's name?","player2")
   document.getElementById("countlabel1").innerHTML= 0
   document.getElementById("countlabel2").innerHTML= 0
 }
@@ -40,7 +36,7 @@ function getRandomValues(){
       }
     
      
-    if (numberOfRound<Round){
+    if (numberOfRoundsPlayed<maxNumberOfRounds){
     const d1Value = randomIntFromInterval(1, 6)
     const d2Value = randomIntFromInterval(1, 6)
     
@@ -51,16 +47,19 @@ function getRandomValues(){
     document.getElementById("countlabel1").innerHTML= scorePlayer1
     scorePlayer2=scorePlayer2+d2Value
     document.getElementById("countlabel2").innerHTML= scorePlayer2
-    numberOfRound++
+    numberOfRoundsPlayed++
 
-    document.getElementById("AcR").innerHTML =numberOfRound
+    document.getElementById("AcR").innerHTML =numberOfRoundsPlayed
     
-    if (numberOfRound==Round){
-      if (scorePlayer2<scorePlayer1){
-        document.getElementById("alert1").innerHTML=palyer1 +" is the winner with score :"+scorePlayer1
+    if (numberOfRoundsPlayed==maxNumberOfRounds){
+      if(scorePlayer1==scorePlayer2){
+        document.getElementById("alert1").innerHTML="Draw :)"
+      }
+      else if (scorePlayer2<scorePlayer1){
+        document.getElementById("alert1").innerHTML=palyer1 +"\nis the winner :)"
       }
       else if (scorePlayer2>scorePlayer1){
-        document.getElementById("alert1").innerHTML=palyer2 +" is the winner  with score :"+scorePlayer2
+        document.getElementById("alert1").innerHTML=palyer2 +"\nis the winner :)"
 
       }
       
